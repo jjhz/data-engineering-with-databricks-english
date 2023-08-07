6c38b08937e0dd201fe48f7473fdbccaeccaedfd
jjhz@outlook.com <jjhz@outlook.com>
2023-08-07 04:02:28 +0000

    test

--- my_delta_live_table/01_create_delta_live_table.sql
+++ my_delta_live_table/01_create_delta_live_table.sql
@@ -10,6 +10,13 @@ AS SELECT * FROM cloud_files("${source}", "csv")
 
 -- COMMAND ----------
 
+-- CREATE OR REFRESH live table my_dtl_test_no_STREAMING
+-- AS SELECT * FROM cloud_files("${source}", "csv")
+
+-- org.apache.spark.sql.AnalysisException: 'my_dtl_test_no_STREAMING' was read as a stream (i.e. using `readStream` or `STREAM(...)`), but 'my_dtl_test_no_STREAMING' is not a streaming table. Either add the STREAMING keyword to the CREATE clause or read the input as a table rather than a stream.
+
+-- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC
 -- MAGIC # But, how to query the Delta Live Table??
