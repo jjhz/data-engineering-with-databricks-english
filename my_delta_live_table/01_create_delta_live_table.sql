f35562b57388b1832efcf5cb6af0646a417dad84
jjhz@outlook.com <jjhz@outlook.com>
2023-08-07 05:07:20 +0000

    test

--- my_delta_live_table/01_create_delta_live_table.sql
+++ my_delta_live_table/01_create_delta_live_table.sql
@@ -5,7 +5,7 @@
 
 -- COMMAND ----------
 
-CREATE OR REFRESH STREAMING TABLE my_dtl_test
+CREATE OR REFRESH STREAMING live TABLE my_dtl_test
 AS SELECT * FROM cloud_files("${source}", "csv")
 
 -- COMMAND ----------
@@ -17,10 +17,17 @@ AS SELECT * FROM cloud_files("${source}", "csv")
 
 -- COMMAND ----------
 
+CREATE OR REFRESH STREAMING live table my_dtl_test_no_STREAMING
+AS SELECT * FROM cloud_files("${source}", "csv")
+
+-- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC
 -- MAGIC # But, how to query the Delta Live Table??
--- MAGIC * You need to config the pipeline put the database name and table name in there
+-- MAGIC * You need to config the pipeline put the database name as Target.
+-- MAGIC * The string you put in the target option will be the database name
+-- MAGIC * All the tables you create in the notebook will be stored in side the database you created abvove
 
 -- COMMAND ----------
 
